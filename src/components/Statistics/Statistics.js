c8809ba69292f0ba7397802e06f639a37cbf4910
FilBelan <filbelan@gmail.com>
2023-04-17 13:01:35 +0300

    final feedback

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import Notification from 'components/Notification/Notification';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
     <>
-      {total > 0 && (
+      {total > 0 ? (
         <>
           <li>Good: {good} </li>
-          <li> Neutral: {neutral} </li>
+          <li>Neutral: {neutral} </li>
           <li>Bad: {bad} </li>
-          <li>Total: {total}</li>
+          <li>Total: {total} </li>
           <li>Positive feedback: {positivePercentage}%</li>
         </>
+      ) : (
+        <Notification message="There is no feedback" />
       )}
     </>
   );
